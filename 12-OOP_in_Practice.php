ac1e75ef6d0e8c44ed41d61e889ffd73cd6d61d5
Thomas Elvin <info@datalaere.dk>
2019-06-30 18:17:45 +0200

    Update and rename 12-OOP_in_Practice to 12-OOP_in_Practice.php

---
+++ 12-OOP_in_Practice.php
@@ -0,0 +1,54 @@
+<?php
+
+/* This is an example of everything we have learne and how to use OOP in practice. Here we will try making an calculator. */
+
+class Calculator {
+
+}
+
+
+interface OperatorInterface {
+
+}
+
+class Adder {
+
+}
+
+
+class Subtractor {
+
+}
+
+
+class Divider {
+
+}
+
+
+class Multiplier {
+
+}
+
+
+// Instantiate
+$c = new Calculator 
+
+// Addition
+$c->setOperation(new Adder);
+$c->calculate(20, 30); // 50
+
+// Subtraction
+$c->setOperation(new Subtractor);
+$c->calculate(5); // 20
+
+// Division
+$c->setOperation(new Divider);
+$c->calculate(2); // 25
+
+// Muliplication
+$c->setOperation(new Multiplier);
+$c->calculate(5); // 100!
+
+echo $c->getResult();
+
