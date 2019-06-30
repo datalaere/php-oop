f99fa16c965289e5227e1ad0ecc0175340343d8e
Thomas Elvin <info@datalaere.dk>
2019-06-30 20:20:00 +0200

    Update 1-Intro.php

--- 1-Intro.php
+++ 1-Intro.php
@@ -4,6 +4,7 @@
 $object = new stdClass; 
 $object->names = ['John', 'Billy', 'Susan', 'Max']; 
 
-foreach($object->names as $name){ 
+foreach($object->names as $name)
+{ 
     echo $name . '<br>'; 
 }
