1e6fccea2efd9553a2e99a0e3bdcc8734c1917ab
Thomas Elvin <info@datalaere.dk>
2019-06-30 18:09:00 +0200

    Update 11-Method_Chaining.php

--- 11-Method_Chaining.php
+++ 11-Method_Chaining.php
@@ -1,1 +1,25 @@
 <?php
+
+/* Method chaining is just a way of using multiple methods together */
+
+class Example {
+
+  public function hello() {
+    echo 'Hello ';
+    
+    // This is the important for getting chaining to work
+    return $this;
+  }
+  
+  public function world() {
+    echo 'World1';
+  }
+
+}
+
+// Instantiate
+$example = new Example();
+
+// Will return "Hello World!"
+$example->hello()->world();
+
