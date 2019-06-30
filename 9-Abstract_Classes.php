68e8ae96b5d2036a61b6c0f6c53baedab43a7ea0
Thomas Elvin <info@datalaere.dk>
2019-06-30 17:24:13 +0200

    Update 9-Abstract_Classes.php

--- 9-Abstract_Classes.php
+++ 9-Abstract_Classes.php
@@ -1,1 +1,42 @@
+<?php
 
+/* Abstract classes are almost like interfaces in design, but are more like a combination of both classes and interfaces. */
+
+abstract class Foo {
+
+  public function bar() {
+    return 'foobar';
+  }
+  
+  abstract function foobar();
+  
+}
+
+// Instantiate
+$foo = new Foo();
+
+// Will return an error, because an abstract class cannot be instantiated.
+echo $foo->bar();
+
+
+class Bar extends Foo {
+
+  public function hello() {
+    return 'Hello World!';
+  }
+  
+  public function foobar() {
+    return 'Foobar!';
+  }
+  
+}
+
+// Instantiate
+$bar = new Bar();
+
+echo $bar->hello();
+// Will also work because we extend the abstract class
+echo $bar->bar();
+
+// Abstract method return "Foobar!"
+echo $bar->foobar();
