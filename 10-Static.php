a270b129cc02ed3f3971d0db480851d7e67ed689
Thomas Elvin <info@datalaere.dk>
2019-06-30 17:32:01 +0200

    Update 10-Static.php

--- 10-Static.php
+++ 10-Static.php
@@ -1,1 +1,25 @@
 <?php
+
+/* Static is a simple concept BUT most be used with care, as it can easily lead to bad code practice. */
+
+class Example {
+  
+  public static $var = 'foobar';
+
+  public static function hello() {
+    return 'Hello World!';
+  }
+  
+   public static function foobar() {
+    return $this->var;
+  }
+
+}
+
+// Can be called without instantiation - returns "Hello World!".
+echo Example::hello();
+// Returns "foobar".
+echo Example::$var;
+
+// This will not work, because the class is NOT instantiated
+echo Example::foobar();
