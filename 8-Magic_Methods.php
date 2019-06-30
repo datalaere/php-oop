6baa38952032e23f3b7437288cef9fc9f5ead259
Thomas Elvin <info@datalaere.dk>
2019-06-30 20:29:23 +0200

    Update 8-Magic_Methods.php

--- 8-Magic_Methods.php
+++ 8-Magic_Methods.php
@@ -14,53 +14,66 @@ The most popular are: */
 
 // Example
 
-class Collection implements Countable, JsonSerializable {
+class Collection implements 
+  Countable, 
+  JsonSerializable 
+{
   // Default properties
   protected $items = [];
   
   // Magic method
-  public function __set($key) {
+  public function __set($key) 
+  {
     $this->set($key, $value);
   }
   
   // Magic method
-  public function __get($value) {
+  public function __get($value) 
+  {
     return $this->get($value);
   }
   
   // Magic method. "__callStatic" is bassically the same for static methods
-  public function __call($func, $args) {
+  public function __call($func, $args) 
+  {
     return $func . ' has been called with arguments' . implode(', ', $args);
   }
   
   // Magic method
-  public function __toString() {
+  public function __toString() 
+  {
     return $this->jsonSerialize();
   }
    
-  public function add($value) {
+  public function add($value) 
+  {
     $this->items[] = $value;
   }
   
-  public function set($key, $value) {
+  public function set($key, $value) 
+  {
     $this->items[$key] = $value;
   }
   
-  public function get($key) {
+  public function get($key) 
+  {
     return array_key_exists($key, $this->items) ? $this->items[$key] : null;
   }
   
   // interface method
-  public function jsonSerialize() {
+  public function jsonSerialize() 
+  {
     return json_encode($this->items);
   }
   
   // interface method
-  public function count() {
+  public function count() 
+  {
     return count($this->items);
   }
   
-  public function all() {
+  public function all() 
+  {
     return $this->items;
   }
   
